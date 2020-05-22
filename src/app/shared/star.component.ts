5da9355aa8ed673108054b20354098f31b32bf54
mmichtch <maxim@4refuel.com>
2020-05-22 14:37:16 -0700

    Implemented component with input data

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+
+})
+export class StarComponent implements OnChanges {
+  @Input()
+  rating: number;
+  starWidth: number;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
+}
