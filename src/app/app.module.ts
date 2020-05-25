eed4fe21d68b1fdab72864e865aac23cb04b49be
mmichtch <maxim@4refuel.com>
2020-05-24 22:57:50 -0700

    Added app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,12 +15,8 @@ import { ProductModule } from './products/product.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'welcome',component: WelcomeComponent },
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'  },
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'  },
-    ],{ useHash: false}),
-    ProductModule
+    ProductModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
